b599d3bce6c400dce7a7b477f161341de6654617
wangteng <517270520@qq.com>
2026-03-25 11:37:30 +0800

    新增根据组合条件查询用户信息列表接口

--- user-server/src/main/java/com/demo/user/UserServerApplication.java
+++ user-server/src/main/java/com/demo/user/UserServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.demo.product.mapper")
+@MapperScan("com.demo.user.mapper")
 public class UserServerApplication {
 
     public static void main(String[] args) {
